74c5a8dcae5f41c596a87962a9f886c67d48303d
RedMousePad <72360193+RedMousePad@users.noreply.github.com>
2020-10-05 03:06:14 +0500

    Updated metadata for source

--- src/MangaDex/MangaDex.ts
+++ src/MangaDex/MangaDex.ts
@@ -7,11 +7,11 @@ export class MangaDex extends Source {
   }
 
   get version(): string { return '2.0.0' }
-  get name(): string { return 'MangaDex' }
+  get name(): string { return 'MangaDex Bypassed' }
   get icon(): string { return 'icon.png' }
-  get author(): string { return 'Faizan Durrani' }
-  get authorWebsite(): string { return 'https://github.com/FaizanDurrani' }
-  get description(): string { return 'Extension that pulls manga from MangaDex, includes Advanced Search and Updated manga fetching' }
+  get author(): string { return 'RedMousePad' }
+  get authorWebsite(): string { return 'https://github.com/RedMousePad' }
+  get description(): string { return 'MangaDex repo with licensed manga filter bypassed' }
   get hentaiSource(): boolean { return false }
   get websiteBaseURL(): string { return 'https://mangadex.org' }
 
